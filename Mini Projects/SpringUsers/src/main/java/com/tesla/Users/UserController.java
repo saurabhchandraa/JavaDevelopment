4a93751c8092c24b7f1fb152adc2c20750ff66b7
tesla2101 <saurabhchandraa@gmail.com>
2021-01-13 14:32:11 +0530

    Created Update and Delete for SpringUser

--- Mini Projects/SpringUsers/src/main/java/com/tesla/Users/UserController.java
+++ Mini Projects/SpringUsers/src/main/java/com/tesla/Users/UserController.java
@@ -2,18 +2,21 @@ package com.tesla.Users;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(path="/")
@@ -45,5 +48,23 @@ public class UserController {
 	userRep.save(UserList);
 	return "redirect:/all";	
 	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/update/{id}")
+	public ModelAndView updateUserById(@PathVariable(name = "id") int id)
+	{
+		ModelAndView mv = new ModelAndView("new");
+		User u = userRep.findById(id).get();
+		mv.addObject("UserList", u);
+		return mv;
+	}
+	
+	@RequestMapping(method=RequestMethod.GET, value="/delete/{id}")
+	public String deleteUserById(@PathVariable(name = "id") int id)
+	{
+		userRep.deleteById(id);
+		return "redirect:/all";
+	}
+	
+	
 
 }
